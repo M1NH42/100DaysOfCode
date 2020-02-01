88ad21ff72ffa8a947da51d6063034d49f2b7871
M1NH42 <minhazalam365@gmail.com>
2020-02-01 21:17:08 +0530

    about the program

---
+++ Day4/string_cpp.cpp
@@ -0,0 +1,7 @@
+/* Program to demonstrate the working of the string
+    which is more efficient than the character array
+    in terms of the memory as it has no fixed memory
+    size
+    - but only drawback is that it has fixed no. elements
+    that means static type, we have to define the number 
+    of elemnets at the compile time */
