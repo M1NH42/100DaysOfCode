a2bb03a6ccc38b57c91c0c2085d1f56b1a4de52d
M1NH42 <minhazalam365@gmail.com>
2020-02-01 19:29:56 +0530

    added comments

--- Day4/arr.cpp
+++ Day4/arr.cpp
@@ -4,9 +4,13 @@ using namespace std;
 // entry point of the program
 int main()
 {
+  //  initialised array with elements
   int arr [4] = {2, 3, 4, 5};
+
+  //loop to print all the elements
   for(int i = 0; i < 4; i++)
   {
+  // &arr[n] shows address of the elements on each iteration
     cout << arr [i] << "at an address of " << &arr [i] <<endl;
   }
   return 0;
